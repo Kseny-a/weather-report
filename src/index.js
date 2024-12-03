6db668a7fa89667b85053df054d197cf08bf51dd
Wei <qwbu22@gmail.com>
2024-12-02 20:27:39 -0800

    wave 2 / dispaly temp in correct color / increase & decrease temp

--- src/index.js
+++ src/index.js
@@ -1,43 +1,50 @@
 const state = {
   tempValue: 70
 }
-const increaseTemp = () => {
-  const tempUp = document.querySelector('#increaseTempControl')
-    state.tempValue +=1
-    const tempContainer = document.querySelector('#tempValue')
-    tempContainer.textContent = state.tempValue;
+
+const tempColorByNum = (value) => {
+
+  if (value >= 80){
+    tempColor = 'red';
+  } else if (value >= 70) {
+    tempColor = 'orange';
+  } else if (value >= 60) {
+    tempColor = 'yellow';
+  } else if (value >= 50) {
+    tempColor = 'green';
+  } else {
+    tempColor = 'teal';
+  }
+  return tempColor
 };
 
 
+const updateTemperatureDisplay = () => {
+  const tempContainer = document.querySelector('#tempValue');
+  tempContainer.textContent = `${state.tempValue}F`;
+  tempContainer.style.color = tempColorByNum(state.tempValue);
+}
+
+const increaseTemp = () => {
+  state.tempValue += 1;
+  updateTemperatureDisplay();
+}
+
+const decreaseTemp = () => {
+  state.tempValue -= 1;
+  updateTemperatureDisplay();
+}
+
 const registerEventHandlers = () => {
-  const tempUp = document.querySelector('#increaseTempControl')
-  tempUp.addEventListener("click", increaseTemp());
-}
+  const tempUpButton = document.querySelector('#increaseTempControl');
+  const tempDownButton = document.querySelector('#decreaseTempControl');
 
-const tempDown = document.querySelector('#decreaseTempControl')
-tempDown.addEventListener('click', (tempValue) => {
-  return tempValue-=1
-});
+  tempUpButton.addEventListener("click", increaseTemp);
+  tempDownButton.addEventListener("click", decreaseTemp);
+};
 
-document.addEventListener("DOMContentLoaded", registerEventHandlers);
+document.addEventListener("DOMContentLoaded", () => {
+  updateTemperatureDisplay();
+  registerEventHandlers();
+})
 
-// let tempValue = 72;
-
-const temp = document.getElementById('tempValue')
-temp.innerText = tempValue
-temp.style.color = tempColorByNum(tempValue)
-const tempColorByNum = (value) => {
-  let tempColor = '';
-if (value > 80){
-  tempColor = 'red'
-} else if (value >= 70) {
-  tempColor = 'orange'
-} else if (value >= 60) {
-  tempColor = 'yellow'
-} else if (value >= 50) {
-  tempColor = 'green'
-} else {
-  tempColor = 'teal'
-}; 
-  return tempColor
-}
